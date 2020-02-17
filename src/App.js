e36f35fae5af82be1983791300d718c0c2e70e43
DHalland <dylanhalland@gmail.com>
2020-02-16 21:26:10 -0800

    outputs country info from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+class App extends Component {
+   state = {
+      country: {}
+   };
+
+   async componentDidMount() {
+      const url = "https://restcountries.eu/rest/v2/name/col";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data[0].name);
+      this.setState({ country: data[0] });
+      //console.log(data);
+   }
+   render() {
+      return (
+         <div>
+            <p>{JSON.stringify(this.state.country)}</p>
+         </div>
+      );
+   }
 }
-
 export default App;
