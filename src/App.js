33a8b89bf77810dd9a1e985456ed77c168aba7a7
DHalland <dylanhalland@gmail.com>
2020-02-17 18:25:15 -0800

    displays list of all countries

--- src/App.js
+++ src/App.js
@@ -2,24 +2,57 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-   state = {
-      country: {}
-   };
-
-   async componentDidMount() {
-      const url = "https://restcountries.eu/rest/v2/name/col";
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data[0].name);
-      this.setState({ country: data[0] });
-      //console.log(data);
+   constructor(props) {
+      super(props);
+      this.state = {
+         countries: []
+      };
    }
+
+   componentDidMount() {
+      fetch("https://restcountries.eu/rest/v2/all")
+         .then(result => result.json())
+         .then(json => {
+            this.setState({
+               countries: json
+            });
+         });
+   }
+
    render() {
+      var { countries } = this.state;
       return (
-         <div>
-            <p>{JSON.stringify(this.state.country)}</p>
+         <div className="App">
+            <ul>
+               {countries.map(country => (
+                  <li key={country.alpha3Code}>{country.name}</li>
+               ))}
+            </ul>
          </div>
       );
    }
+
+   // THE COMMENTED CODE BELOW IS ANOTHER WAY OF DOING THE SAME THING
+   //
+   //  state = {
+   //     countries: []
+   //  };
+
+   //  async componentDidMount() {
+   //     const url = "https://restcountries.eu/rest/v2/name/col";
+   //     const response = await fetch(url);
+   //     const data = await response.json();
+   //     console.log(data[0].name);
+   //     this.setState({ country: data[0] });
+   //     //console.log(data);
+   //  }
+
+   //  render() {
+   //    return (
+   //      <div>
+   //        <p>{JSON.stringify(this.state.countries)}</p>
+   //      </div>
+   //    )
+   //  }
 }
 export default App;
