16c6c9062ff8a229917b814e8d26db08efc41635
Keith Gordon <keithagordon6@gmail.com>
2020-02-20 21:10:40 -0800

    fixed css for more info section

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -8,7 +8,7 @@ class SearchBar extends Component {
       return (
          <div className="container-fluid">
             <div id="header" className="row">
-               <div id="name" className="col-4"><h1>{this.props.name}</h1></div>
+               <div id="name" className="col-4"><h3>{this.props.name}</h3></div>
                
                <div className="form-group col-8">
                   <label  id="search" htmlFor="formGroupExampleInput"></label>
